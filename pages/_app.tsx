5d2839ee87e2b051dfbad8df263f139114972c2d
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-28 07:12:38 +0100

    ajouter le Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,14 @@ import type {AppProps} from 'next/app'
 import {RecoilRoot} from "recoil";
 import {AppBar} from '../components/app.bar';
 import BottomBar from "../components/bottom.bar";
+import Head from "next/head";
 
 export default function App({Component, pageProps}: AppProps) {
     return <>
         <RecoilRoot>
+            <Head>
+                <title>Kru</title>
+            </Head>
             <AppBar title={"Kru"}/>
             <Component {...pageProps} />
             <BottomBar/>
