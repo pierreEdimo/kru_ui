73173a0e25949b8e22081819ad890577d9e76c23
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-28 04:59:40 +0100

    ajouter le bottomnav pour l'affichage mobile

---
+++ components/bottom.bar.tsx
@@ -0,0 +1,40 @@
+import styles from "../styles/bottom.bar.module.scss";
+import React from "react";
+import {Layout} from "./layout";
+import home from "../public/home.png";
+import operation from "../public/operation.png";
+import wallet from "../public/wallet.png";
+import transaction from "../public/transaction.png";
+import Link from "next/link";
+import Image from "next/image";
+
+
+interface BottomBarProps {
+}
+
+const BottomBar: React.FC<BottomBarProps> = () => {
+    return (
+        <>
+            <div className={styles.bottomBar}>
+                <Layout>
+                    <div className={styles.bottomBarContent}>
+                        <Link href={"/"}>
+                            <Image src={home} alt={"home-image"} width={35} height={35}/>
+                        </Link>
+                        <Link href={"/"}>
+                            <Image src={operation} alt={"paiement-image"} width={35} height={35}/>
+                        </Link>
+                        <Link href={"/"}>
+                            <Image src={wallet} alt={"wallet-image"} width={35} height={35}/>
+                        </Link>
+                        <Link href={"/"}>
+                            <Image src={transaction} alt={"transaction-image"} width={35} height={35}/>
+                        </Link>
+                    </div>
+                </Layout>
+            </div>
+        </>
+    )
+}
+
+export default BottomBar;
