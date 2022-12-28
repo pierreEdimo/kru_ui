d78ce2b3ad5665875938e3f1526b3dcaeb2cb661
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-28 04:58:31 +0100

    creer le appbar

---
+++ components/app.bar.tsx
@@ -0,0 +1,47 @@
+import styles from "../styles/app.bar.module.scss";
+import {Layout} from "./layout";
+import Link from "next/link";
+import bell from "../public/notification (1).png";
+import Image from "next/image";
+import user from "../public/user (1).png";
+import settings from "../public/settings.png";
+
+export function AppBar({title}: { title: string }) {
+    return (
+        <header className={styles.appHeader}>
+            <Layout>
+                <div className={styles.appHeaderContent}>
+                    <div className={styles.menuItems}>
+                        <h2 className={styles.title}>{title}</h2>
+                        <Link href="/">Dashboard</Link>
+                        <Link href="/">Paiement</Link>
+                        <Link href="/">Portefeuilles</Link>
+                        <Link href="/">Transactions</Link>
+                    </div>
+                    <div className={styles.menuIcons}>
+                        <Link href="/">
+                            <Image src={bell} alt={"notification-image"} width={25} height={25}/>
+                        </Link>
+                        <Link href="/">
+                            <Image src={settings} alt={"setting-image"} width={25} height={25}/>
+                        </Link>
+                        <Link href="/">
+                            <Image src={user} alt={"user-image"} width={25} height={25}/>
+                        </Link>
+                    </div>
+                </div>
+                <div className={styles.mobileAppHeaderContent}>
+                    <h2 className={styles.title}>{title}</h2>
+                    <div className={styles.menuIcons}>
+                        <Link href="/">
+                            <Image src={settings} alt={"setting-image"} width={25} height={25}/>
+                        </Link>
+                        <Link href="/">
+                            <Image src={user} alt={"user-image"} width={25} height={25}/>
+                        </Link>
+                    </div>
+                </div>
+            </Layout>
+        </header>
+    );
+}
