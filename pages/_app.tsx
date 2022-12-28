35a1fec29a6d629378a11c0ac2e1c4db5b4dd16a
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-28 05:03:29 +0100

    ajouter le RecoilRoot

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss'
+import type {AppProps} from 'next/app'
+import {RecoilRoot} from "recoil";
+import {AppBar} from '../components/app.bar';
+import BottomBar from "../components/bottom.bar";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({Component, pageProps}: AppProps) {
+    return <>
+        <RecoilRoot>
+            <AppBar title={"Kru"}/>
+            <Component {...pageProps} />
+            <BottomBar/>
+        </RecoilRoot>
+    </>
 }
