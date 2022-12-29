2ddd51552610e1e3431276a5415180995a5a16ea
pierre Edimo <pedimonkoe@yahoo.com>
2022-12-29 03:16:09 +0100

    creer la page historique

---
+++ pages/historique.tsx
@@ -0,0 +1,14 @@
+import {InnerLayout} from "../components/inner.layout";
+import {Layout} from "../components/layout";
+
+export default function Historique() {
+    return (
+        <>
+            <Layout>
+                <InnerLayout>
+                    <p>Hello historique</p>
+                </InnerLayout>
+            </Layout>
+        </>
+    )
+}
